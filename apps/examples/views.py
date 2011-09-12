026aa67a03d6256f5c2fea98413a6107dbec861e
MikeTheTechie <mike2010@austin-uk.co.uk>
2011-09-12 18:22:15 +0100

    added a test comment

--- apps/examples/views.py
+++ apps/examples/views.py
@@ -13,7 +13,7 @@ log = commonware.log.getLogger('playdoh')
 def home(request):
     """Main example view."""
     data = {}  # You'd add data here that you're sending to the template.
-    log.debug("I'm alive now!")
+    log.debug("I'm alive now!") # another test
     return jingo.render(request, 'examples/home.html', data)
 
 
